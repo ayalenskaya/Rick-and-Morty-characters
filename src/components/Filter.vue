b0db7a279bfb6eb7c29aab1026eee4f2f58731a9
ayalenskaya <kamio-vk@yandex.ru>
2024-05-14 12:24:19 +0300

    Filter template

---
+++ src/components/Filter.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="filter-container">
+    <input v-model="filter.name" type="text" placeholder="Filter by name" />
+    <select v-model="filter.status">
+      <option value="">All</option>
+      <option value="alive">Alive</option>
+      <option value="dead">Dead</option>
+      <option value="unknown">Unknown</option>
+    </select>
+    <button @click="applyFilters">Apply</button>
+  </div>
+</template>
