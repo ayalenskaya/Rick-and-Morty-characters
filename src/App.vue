3195a9848e94228836cb2172a9b003fe8a0eec23
ayalenskaya <kamio-vk@yandex.ru>
2024-05-14 12:27:17 +0300

    App template

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="character-container">
+    <h1>Rick and Morty Characters</h1>
+    <Filter @applyFilters="applyFilters" />
+    <div class="character-grid">
+      <CharacterCard v-for="character in characters" :key="character.id" :character="character" />
+    </div>
+    <Pagination :page="page" :totalPages="totalPages" @nextPage="nextPage" @prevPage="prevPage" />
+  </div>
+</template>
+
