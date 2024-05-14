a15c46d5aa2e54d032c07832d9b0207c48741e90
ayalenskaya <kamio-vk@yandex.ru>
2024-05-14 12:21:12 +0300

    Card template

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="character-card">
+      <div class="character-content">
+        <img :src="character.image" :alt="character.name" class="character-image" />
+        <div class="character-details">
+          <h2>{{ character.name }}</h2>
+          <p>{{ character.status }} - {{ character.species }}</p>
+          <p>Last known location: {{ character.origin.name }}</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
