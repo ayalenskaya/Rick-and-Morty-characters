e545199ee82bc895595110f97ca9bd5cd876443c
ayalenskaya <kamio-vk@yandex.ru>
2024-05-14 12:32:38 +0300

    Deploy to GitHub Pages

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="character-card">
+      <div class="character-content">
+        <img :src="character.image" :alt="character.name" class="character-image" />
+        <div class="character-details">
+          <h2>{{ character.name }}</h2>
+          <p>{{ character.status }} - {{ character.species }}</p>
+          <p>Last known location: {{ character.origin.name }}</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    character: Object
+  });
+  </script>
+  
+  <style scoped>
+  .character-card {
+    width: clamp(300px, 80%, 500px); 
+    display: flex;
+    background: rgb(60, 62, 68);
+    border-radius: 0.5rem;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
+  }
+  
+  .character-content {
+    display: flex;
+    align-items: center;
+  }
+  
+  .character-details {
+    padding: 1rem;
+    text-align: left;
+  }
+
+  @media screen and (max-width: 670px) {
+  .character-content {
+    flex-direction: column;
+    width: 100%;
+  }
+  .character-card {
+    justify-content: center;
+  }
+
+}
+  </style>
+  
---
+++ src/components/Filter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="filter-container">
+    <input v-model="filter.name" type="text" placeholder="Filter by name" />
+    <select v-model="filter.status">
+      <option value="">All</option>
+      <option value="alive">Alive</option>
+      <option value="dead">Dead</option>
+      <option value="unknown">Unknown</option>
+    </select>
+    <button @click="applyFilters">Apply</button>
+  </div>
+</template>
+
+<script setup>
+import { reactive, defineEmits } from "vue";
+
+const filter = reactive({
+  name: "",
+  status: "",
+});
+
+const emit = defineEmits(["applyFilters"]);
+
+const applyFilters = () => {
+  emit("applyFilters", filter);
+};
+</script>
+
+<style scoped>
+.filter-container {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+</style>
