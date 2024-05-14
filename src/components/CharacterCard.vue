e19d870d8b5fa43027a5bf71cc5f74534092fea7
ayalenskaya <kamio-vk@yandex.ru>
2024-05-14 12:22:02 +0300

    Card style

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -17,3 +17,34 @@
   });
   </script>
   
+  <style scoped>
+  .character-card {
+    width: clamp(300px, 80%, 500px); 
+    display: flex;
+    background: rgb(60, 62, 68);
+    border-radius: 0.5rem;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
+  }
+  
+  .character-content {
+    display: flex;
+    align-items: center;
+  }
+  
+  .character-details {
+    padding: 1rem;
+    text-align: left;
+  }
+
+  @media screen and (max-width: 670px) {
+  .character-content {
+    flex-direction: column;
+    width: 100%;
+  }
+  .character-card {
+    justify-content: center;
+  }
+
+}
+  </style>
+  
