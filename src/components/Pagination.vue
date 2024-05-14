401ca9201d899afb8776a6259e5b3cff9eed9776
ayalenskaya <kamio-vk@yandex.ru>
2024-05-14 12:17:51 +0300

    pagination template

---
+++ src/components/Pagination.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="pagination">
+      <button @click="prevPage" :disabled="page <= 1">Previous</button>
+      <button @click="nextPage" :disabled="page >= totalPages">Next</button>
+    </div>
+  </template>
+  
