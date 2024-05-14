e867f912a280b59d933df6adb9bdc53f5b3bdf4f
ayalenskaya <kamio-vk@yandex.ru>
2024-05-14 12:24:37 +0300

    Filter script

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -10,3 +10,18 @@
     <button @click="applyFilters">Apply</button>
   </div>
 </template>
+
+<script setup>
+import { reactive, defineEmits } from "vue";
+
+const filter = reactive({
+  name: "",
+  status: "",
+});
+
+const emit = defineEmits(["applyFilters"]);
+
+const applyFilters = () => {
+  emit("applyFilters", filter);
+};
+</script>
